e95e1e5368b01b1e4fd01094cd5e0073c324a719
carlosivis <52825816+carlosivis@users.noreply.github.com>
2019-11-07 22:23:24 -0300

    Add files via upload

---
+++ Formas/src/formas/Formas.java
@@ -0,0 +1,12 @@
+
+package formas;
+
+/**
+ *
+ * @author carlos
+ */
+abstract public class Formas {
+    abstract public double calculaArea();
+    abstract public double calculaVolume();
+    
+}
